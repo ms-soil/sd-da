afaa66e3343ef120a5b587e79ba8b72d3768a160
ms-soil <mschmidh@gwdg.de>
2020-03-25 21:08:35 +0100

    first commit

---
+++ analysis.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+
+#### view instructions ####
+# read_file("instructions")
+
+#### view variable info ####
+# View(read_csv("data/Eucalypt_Variables.csv"))
+
+#### get data ####
+d <- read_csv("data/Euc_data.csv")
+# View(d)
+head(d)
