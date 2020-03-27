ce5712f0d9304f85cb38691263ca016fd1d09bfd
BJBerauer <bernd.berauer@gmx.de>
2020-03-27 12:38:34 +0100

    Also just trying to make changes... Ich stelle fest das mein Laptop von zu Hause aus sehrschlecht mit dem Remote-Server der Uni kommuniziert

--- analysis.R
+++ analysis.R
@@ -1,12 +1,21 @@
+########################################################
+### Same Data - Different Analyst
+### Question: "How does grass cover influence Eucalyptus spp. seedling recruitment?"
+### Group: BonaRes R-Group
+### Participants: Marcus Schmidt, Paul-David Klein, Bernd J. Berauer
+### Date: wann immer wir Zeit und Lust haben
+########################################################
+
 # How does grass cover influence Eucalyptus spp. seedling recruitment?
 
 library(tidyverse)
 
 #### view instructions ####
-# read_file("instructions")
+read_file("instructions")
 
 #### view variable info ####
-# View(read_csv("data/Eucalypt_Variables.csv"))
+ View(read_csv("data/Eucalypt_Variables.csv"))
+var_info <- read.csv("data/Eucalypt_Variables.csv") #BJB likes to store it :)
 
 #### get data ####
 d <- read_csv("data/Euc_data.csv")
@@ -14,3 +23,4 @@ d <- read_csv("data/Euc_data.csv")
 head(d)
 
 ### why not use: print(tbl_df(d), n=20) ? ;)
+### because its more to write and maybe takes longer to calculate
